7120d66608501358100591f58d64288424382af1
jacobfain <jfain@chicagobooth.edu>
2021-04-21 21:30:04 +0000

    finished add result

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  
+  def blank_add_form
+    render({:template => "calculation_templates/add_form.html.erb"})
+  end
+   
+  
   def add_results
+    @number1 = params.fetch("user_number_one").to_f
+    @number2 = params.fetch("user_number_two").to_f
+    @add = @number1 + @number2 
     render({:template => "calculation_templates/add_results.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get("/add/new",:controller => "application", :action => "blank_add_form")
   get("/add/results",:controller=>"application",:action=>"add_results")
 end
